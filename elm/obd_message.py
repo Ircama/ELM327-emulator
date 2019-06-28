b39224fb2a8b8dbdfe021bd01e15206dcd124c5f
Ircama <amacri@tiscali.it>
2019-06-28 09:04:52 +0200

    Provide initial support for Mode 9 PIDs

--- elm/obd_message.py
+++ elm/obd_message.py
@@ -400,6 +400,33 @@ ObdMessage = {
             'Request': '^06A0' + ELM_MAX_RESP,
             'Descr': 'MIDS_F',
             'Response': ECU_R_ADDR_E + ' 06 46 A0 F8 00 00 00 \r'
+        },
+        'ELM_PIDS_9A': {
+            'Request': '^0900' + ELM_MAX_RESP,
+            'Descr': 'PIDS_9A',
+            'Response': ECU_R_ADDR_E + ' 06 49 00 FF FF FF FF \r'
+        },
+        'VIN_MESSAGE_COUNT': {
+            'Request': '^0901' + ELM_MAX_RESP,
+            'Descr': 'VIN Message Count',
+            'Response': ECU_R_ADDR_E + ' 03 49 01 01 \r'
+        },
+        'VIN': { # Check this also: https://stackoverflow.com/a/26752855/10598800, https://www.autocheck.com/vehiclehistory/autocheck/en/vinbasics
+            'Request': '^0902' + ELM_MAX_RESP,
+            'Descr': 'Get Vehicle Identification Number',
+            'Response': [
+                        ECU_R_ADDR_E + ' 10 14 49 02 01 57 50 30 \r' +
+                        ECU_R_ADDR_E + ' 21 5A 5A 5A 39 39 5A 54 \r' +
+                        ECU_R_ADDR_E + ' 22 53 33 39 32 31 32 34 \r', # https://www.autodna.com/vin/WP0ZZZ99ZTS392124, https://it.vin-info.com/libro-denuncia/WP0ZZZ99ZTS392124
+                        ECU_R_ADDR_E + ' 10 14 49 02 01 4D 41 54 \r' + # https://community.carloop.io/t/how-to-request-vin/153/11
+                        ECU_R_ADDR_E + ' 21 34 30 33 30 39 36 42 \r' +
+                        ECU_R_ADDR_E + ' 22 4E 4C 30 30 30 31 35 \r'
+                        ]
+        },
+        'CALIBRATION_ID_MESSAGE_COUNT': {
+            'Request': '^0903' + ELM_MAX_RESP,
+            'Descr': 'Calibration ID message count for PID 04',
+            'Response': ECU_R_ADDR_E + ' 03 49 03 01 \r'
         }
     },
     'car': {
